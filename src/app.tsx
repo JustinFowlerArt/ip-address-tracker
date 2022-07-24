ee47a390b8c8740092b97dc0ed6f50de5ec0fb90
Justin Fowler <jusfowle@gmail.com>
2022-07-23 22:12:47 -0400

    Remove unnecessary dependencies, add start files

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,27 @@
+import { useState } from 'react';
+
 export const App = () => {
+	const [search, setSearch] = useState('');
+
+	const handleChange = (ipAddress: string) => {
+		setSearch(ipAddress);
+	};
 	return (
-		<div>
-			<h1 id='title' className='text-3xl font-bold underline'>
-				Hello World
-			</h1>
+		<div className='h-screen'>
+			<div
+				className='h-1/3 flex flex-col justify-center items-center'
+				style={{ backgroundImage: 'url(/images/pattern-bg.png)' }}
+			>
+				<h1 id='title' className='text-3xl font-bold'></h1>
+				<form>
+					<input
+						type='text'
+						value={search}
+						className=''
+						onChange={e => handleChange(e.target.value)}
+					></input>
+				</form>
+			</div>
 		</div>
 	);
 };
